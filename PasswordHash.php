12cca4e3aebc0f920be652e5cf548746220c44b1
Baldur Rensch <baldur.rensch@hautelook.com>
2012-08-31 09:46:46 -0700

    Added docblocks and comments

--- PasswordHash.php
+++ PasswordHash.php
@@ -1,38 +1,46 @@
 <?php
 
 namespace Openwall\Phpass;
-#
-# Portable PHP password hashing framework.
-#
-# Version 0.3 / genuine.
-#
-# Written by Solar Designer <solar at openwall.com> in 2004-2006 and placed in
-# the public domain.  Revised in subsequent years, still public domain.
-#
-# There's absolutely no warranty.
-#
-# The homepage URL for this framework is:
-#
-#	http://www.openwall.com/phpass/
-#
-# Please be sure to update the Version line if you edit this file in any way.
-# It is suggested that you leave the main version number intact, but indicate
-# your project name (after the slash) and add your own revision information.
-#
-# Please do not change the "private" password hashing method implemented in
-# here, thereby making your hashes incompatible.  However, if you must, please
-# change the hash type identifier (the "$P$") to something different.
-#
-# Obviously, since this code is in the public domain, the above are not
-# requirements (there can be none), but merely suggestions.
-#
 
+/**
+ *
+ * Portable PHP password hashing framework.
+ *
+ * Version 0.3 / genuine.
+ *
+ * Written by Solar Designer <solar at openwall.com> in 2004-2006 and placed in
+ *
+ * There's absolutely no warranty.
+ *
+ * The homepage URL for this framework is:
+ *
+ *	http://www.openwall.com/phpass/
+ *
+ * Please be sure to update the Version line if you edit this file in any way.
+ * It is suggested that you leave the main version number intact, but indicate
+ * your project name (after the slash) and add your own revision information.
+ *
+ * Please do not change the "private" password hashing method implemented in
+ * here, thereby making your hashes incompatible.  However, if you must, please
+ * change the hash type identifier (the "$P$") to something different.
+ *
+ * Obviously, since this code is in the public domain, the above are not
+ * requirements (there can be none), but merely suggestions.
+ *
+ * @author Solar Designer <solar@openwall.com>
+ */
 class PasswordHash {
 	private $itoa64;
 	private $iteration_count_log2;
 	private $portable_hashes;
 	private $random_state;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param int $iteration_count_log2
+	 * @param boolean $portable_hashes
+	 */
 	public function __construct($iteration_count_log2, $portable_hashes)
 	{
 		$this->itoa64 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
@@ -50,6 +58,10 @@ class PasswordHash {
 		}
 	}
 
+	/**
+	 * @param  int $count
+	 * @return String
+	 */
 	public function get_random_bytes($count)
 	{
 		$output = '';
@@ -73,6 +85,11 @@ class PasswordHash {
 		return $output;
 	}
 
+	/**
+	 * @param  String $input
+	 * @param  int $count 
+	 * @return String
+	 */
 	public function encode64($input, $count)
 	{
 		$output = '';
@@ -100,6 +117,10 @@ class PasswordHash {
 		return $output;
 	}
 
+	/**
+	 * @param  String $input
+	 * @return String
+	 */
 	public function gensalt_private($input)
 	{
 		$output = '$P$';
@@ -110,6 +131,11 @@ class PasswordHash {
 		return $output;
 	}
 
+	/**
+	 * @param  String $password
+	 * @param  String $setting
+	 * @return String
+	 */
 	public function crypt_private($password, $setting)
 	{
 		$output = '*0';
@@ -135,12 +161,12 @@ class PasswordHash {
 			return $output;
 		}
 
-		# We're kind of forced to use MD5 here since it's the only
-		# cryptographic primitive available in all versions of PHP
-		# currently in use.  To implement our own low-level crypto
-		# in PHP would result in much worse performance and
-		# consequently in lower iteration counts and hashes that are
-		# quicker to crack (by non-PHP code).
+		// We're kind of forced to use MD5 here since it's the only
+		// cryptographic primitive available in all versions of PHP
+		// currently in use.  To implement our own low-level crypto
+		// in PHP would result in much worse performance and
+		// consequently in lower iteration counts and hashes that are
+		// quicker to crack (by non-PHP code).
 		if (PHP_VERSION >= '5') {
 			$hash = md5($salt . $password, TRUE);
 			do {
@@ -159,11 +185,15 @@ class PasswordHash {
 		return $output;
 	}
 
+	/**
+	 * @param  String $input
+	 * @return String
+	 */
 	public function gensalt_extended($input)
 	{
 		$count_log2 = min($this->iteration_count_log2 + 8, 24);
-		# This should be odd to not reveal weak DES keys, and the
-		# maximum valid value is (2**24 - 1) which is odd anyway.
+		// This should be odd to not reveal weak DES keys, and the
+		// maximum valid value is (2**24 - 1) which is odd anyway.
 		$count = (1 << $count_log2) - 1;
 
 		$output = '_';
@@ -177,16 +207,20 @@ class PasswordHash {
 		return $output;
 	}
 
+	/**
+	 * @param  String $input
+	 * @return String
+	 */
 	public function gensalt_blowfish($input)
 	{
-		# This one needs to use a different order of characters and a
-		# different encoding scheme from the one in encode64() above.
-		# We care because the last character in our encoded string will
-		# only represent 2 bits.  While two known implementations of
-		# bcrypt will happily accept and correct a salt string which
-		# has the 4 unused bits set to non-zero, we do not want to take
-		# chances and we also do not want to waste an additional byte
-		# of entropy.
+		// This one needs to use a different order of characters and a
+		// different encoding scheme from the one in encode64() above.
+		// We care because the last character in our encoded string will
+		// only represent 2 bits.  While two known implementations of
+		// bcrypt will happily accept and correct a salt string which
+		// has the 4 unused bits set to non-zero, we do not want to take
+		// chances and we also do not want to waste an additional byte
+		// of entropy.
 		$itoa64 = './ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 		$output = '$2a$';
@@ -218,6 +252,9 @@ class PasswordHash {
 		return $output;
 	}
 
+	/**
+	 * @param String $password
+	 */
 	public function HashPassword($password)
 	{
 		$random = '';
@@ -253,12 +290,17 @@ class PasswordHash {
 			return $hash;
 		}
 
-		# Returning '*' on error is safe here, but would _not_ be safe
-		# in a crypt(3)-like function used _both_ for generating new
-		# hashes and for validating passwords against existing hashes.
+		// Returning '*' on error is safe here, but would _not_ be safe
+		// in a crypt(3)-like function used _both_ for generating new
+		// hashes and for validating passwords against existing hashes.
 		return '*';
 	}
 
+	/**
+	 * @param String $password
+	 * @param String $stored_hash
+	 * @return boolean
+	 */
 	public function CheckPassword($password, $stored_hash)
 	{
 		$hash = $this->crypt_private($password, $stored_hash);
@@ -269,5 +311,3 @@ class PasswordHash {
 		return $hash == $stored_hash;
 	}
 }
-
-?>
