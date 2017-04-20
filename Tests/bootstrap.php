963b888d3ccb12ea708857634724648f13bcbabc
ozh <ozh@ozh.org>
2017-04-20 17:59:26 +0200

    Forward compatibility of tests with latest PHPunit

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -12,3 +12,12 @@ if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php'))) {
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
         'php composer.phar install'.PHP_EOL);
 }
+
+// PHPUnit 6 compatibility for previous versions
+if ( class_exists( 'PHPUnit\Runner\Version' ) && version_compare( PHPUnit\Runner\Version::id(), '6.0', '>=' ) ) {
+    class_alias( 'PHPUnit\Framework\Assert',        'PHPUnit_Framework_Assert' );
+    class_alias( 'PHPUnit\Framework\TestCase',      'PHPUnit_Framework_TestCase' );
+    class_alias( 'PHPUnit\Framework\Error\Error',   'PHPUnit_Framework_Error' );
+    class_alias( 'PHPUnit\Framework\Error\Notice',  'PHPUnit_Framework_Error_Notice' );
+    class_alias( 'PHPUnit\Framework\Error\Warning', 'PHPUnit_Framework_Error_Warning' );
+}
